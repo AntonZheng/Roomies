73095ce1370c51fb1a5f9e4c67a55adefc1e11d0
antonzheng <anton.zheng@gmail.com>
2018-03-11 19:13:56 -0700

    tasks upload to site

--- Roomies/Roomies/TaskViewController.swift
+++ Roomies/Roomies/TaskViewController.swift
@@ -12,43 +12,62 @@ import UIKit
 class TaskViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return roomies.count
     }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 0
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return roomies[row]
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        
+        //print(roomies)
+        roomie = roomies[row]
+        //print(roomie)
     }
     
     
+    @IBOutlet weak var roomiePicker: UIPickerView!
+    @IBOutlet weak var taskName: UITextField!
+    @IBOutlet weak var taskLabel: UILabel!
     // group that must be passed for firebase input
     var group = ""
     var roomies : [String] = []
     var roomie = ""
-    @IBOutlet weak var roomiePicker: UIPickerView!
     var dbReference: DatabaseReference?
-    @IBOutlet weak var taskLabel: UILabel!
-    
     func incoming(group: String) {
         self.group = group
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.roomiePicker.delegate = self
-        self.roomiePicker.dataSource = self
         dbReference = Database.database().reference()
         let refRoomies = dbReference?.child("groups").child(group).child("users")
         refRoomies?.observe(DataEventType.value, with: {(snapshot) in
             if snapshot.childrenCount > 0 {
-                self.roomies.removeAll()
-                print(snapshot.children)
+                let snap = snapshot.value as? [String:[String:String]]
+                for user in (snap?.values)!{
+                    self.roomies.append(user["username"]!)
+                    //print(user["username"]!)
+                }
+                //print("\(self.roomies) v1")
             }
+            //print("\(self.roomies) v2")
+            self.roomiePicker.dataSource = self
+            self.roomiePicker.delegate = self
         })
+        //print("\(self.roomies) v3")
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addTask(_ sender: Any) {
+        if taskName.text!.count > 0 {
+            dbReference?.child("groups").child(self.group).child("tasks").child(taskName.text!).setValue(["Roomie": roomie])
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -56,9 +75,6 @@ class TaskViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDa
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func addTask(_ sender: Any) {
-        
-    }
     
     /*
      // MARK: - Navigation
